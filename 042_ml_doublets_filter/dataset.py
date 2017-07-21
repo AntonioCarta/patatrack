43d3b39e1fed1e0aaabe2ec4faff506a796bb9be
Antonio <w-32@hotmail.it>
2017-07-21 10:47:07 +0200

    load PU35 files. Better angular correction

--- 042_ml_doublets_filter/dataset.py
+++ 042_ml_doublets_filter/dataset.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -5,6 +6,9 @@ from keras.utils.np_utils import to_categorical
 
 
 target_lab = "pdgId"
+
+trainfiles = ['data/PU35/1_' + str(i) + '_Dataset.npy' for i in range(1, 390)]
+
 datalabs = ["run", "evt", "detSeqIn", "detSeqOut", "inZ", "inX", "inY", "outZ",
             "outX", "outY", "detCounterIn", "detCounterOut", "isBarrelIn", "isBarrelOut",
             "layerIn", "ladderIn", "moduleIn", "sideIn", "diskIn", "panelIn", "bladeIn",
@@ -64,29 +68,23 @@ targetlabes = ["dummyFlag", "idTrack", "px", "py", "pz", "pt", "mT", "eT", "mSqr
 
 class Dataset:
     """ Load the dataset from txt files. """
-    def __init__(self, fname, delimit='\t', max_rows=None):
-        with open(fname, 'rb') as f:
-            data = np.load(f)
-            # Compressed .npz files return a dictionary
-            if type(data) != np.ndarray:
-                data = data['arr_0']
-            if max_rows:
-                data = data[:max_rows, :]
-            self.data = pd.DataFrame(data, columns=datalabs)
-
-    def phi_correction(self, hits_in, hits_out):
-        #phi correction
-        cosPhiIns = np.cos(np.arctan(np.multiply(self.data["inY"], 1.0/self.data["inX"])))
-        cosPhiOuts = np.cos(np.arctan(np.multiply(self.data["outY"], 1.0/self.data["outX"])))
-        sinPhiIns = np.sin(np.arctan(np.multiply(self.data["inY"], 1.0/self.data["inX"])))
-        sinPhiOuts = np.sin(np.arctan(np.multiply(self.data["outY"], 1.0/self.data["outX"])))
-
-        inPhiModC = np.multiply(hits_in,cosPhiIns[:,np.newaxis])
-        outPhiModC = np.multiply(hits_out,cosPhiOuts[:,np.newaxis])
-
-        inPhiC  = hits_in  + inPhiModC
-        outPhiC = hits_out + outPhiModC
-        return inPhiC, outPhiC
+    def __init__(self, fname, delimit='\t', max_rows=None, mode='train'):
+        if type(fname) == list:  # Load from directory            
+            l = []
+            for file in fname:
+                print("loading: " + file)
+                a = np.load(file)
+                l.append(a)
+            data = np.vstack(l)
+        else:
+            with open(fname, 'rb') as f:
+                data = np.load(f)
+        # Compressed .npz files return a dictionary
+        if type(data) != np.ndarray:
+            data = data['arr_0']
+        if max_rows:
+            data = data[:max_rows, :]
+        self.data = pd.DataFrame(data, columns=datalabs)
 
     def theta_correction(self, hits_in, hits_out):
         #theta correction
@@ -95,14 +93,22 @@ class Dataset:
         sinThetaIns = np.sin(np.arctan(np.multiply(self.data["inY"],1.0/self.data["inZ"])))
         sinThetaOuts = np.sin(np.arctan(np.multiply(self.data["outY"],1.0/self.data["outZ"])))
 
-        inThetaModC = np.multiply(hits_in,cosThetaIns[:,np.newaxis])
-        outThetaModC = np.multiply(hits_out,cosThetaOuts[:,np.newaxis])
+        inThetaModC = np.multiply(hits_in, cosThetaIns[:,np.newaxis])
+        outThetaModC = np.multiply(hits_out, cosThetaOuts[:,np.newaxis])
 
-        inThetaC  = hits_in  + inThetaModC
-        outThetaC = hits_out + outThetaModC
-        return inThetaC, outThetaC
+        inThetaModS = np.multiply(hits_in, sinThetaIns[:, np.newaxis])
+        outThetaModS = np.multiply(hits_out, sinThetaOuts[:, np.newaxis])
+        return inThetaModC, outThetaModC, inThetaModS, outThetaModS
 
-    def get_hit_shapes(self, normalize=True, angular_correction=True):
+    def separate_flipped_hits(self, hit_shapes, flipped):
+        flipped = flipped.astype('bool')
+        flipped_hits = np.zeros(hit_shapes.shape)
+        not_flipped_hits = np.zeros(hit_shapes.shape)
+        flipped_hits[flipped, :] = hit_shapes[flipped, :]
+        not_flipped_hits[~flipped, :] = hit_shapes[~flipped, :]
+        return flipped_hits, not_flipped_hits
+
+    def get_hit_shapes(self, normalize=True, angular_correction=True, flipped_channels=True):
         """ Return hit shape features
         Args:
         -----
@@ -113,16 +119,20 @@ class Dataset:
         a_out = self.data[outhitlabs].as_matrix()
 
         # Normalize data
-        if normalize:
-            mean, std = (668.25684, 3919.5576) # mean, std precomputed for data NOPU 
-            a_in = (a_in - mean) / std
-            a_out = (a_out - mean) / std
-
-        l = [a_in, a_out]        
+        if normalize:            
+            mean, std = (668.25684, 3919.5576)  # mean, std precomputed for data NOPU
+            a_in = a_in / std
+            a_out = a_out / std
+        
+        if flipped_channels:
+            flip_in, not_flip_in = self.separate_flipped_hits(a_in, self.data.isFlippedIn)
+            flip_out, not_flip_out = self.separate_flipped_hits(a_out, self.data.isFlippedOut)
+            l = [flip_in, not_flip_in, flip_out, not_flip_out]
+        else:
+            l = [a_in, a_out]        
         if angular_correction:
-            phi_in, phi_out = self.phi_correction(a_in, a_out)
             theta_in, theta_out = self.theta_correction(a_in, a_out)
-            data = l + [phi_in, phi_out, theta_in, theta_out]
+            l = l + [phi_in, phi_out, theta_in, theta_out]        
 
         data = np.array(l) # (channels, batch_size, hit_size)
         data = data.reshape((len(data), -1, 8, 8))
@@ -137,11 +147,39 @@ class Dataset:
         """ Returns info features as numpy array. """
         return self.data[featurelabs].as_matrix()
 
+    def get_barrel_data(self):
+        a_in = self.data[inhitlabs].as_matrix()
+        a_out = self.data[outhitlabs].as_matrix()
+
+        mean, std = (668.25684, 3919.5576)  # mean, std precomputed for data NOPU
+        a_in = a_in / std
+        a_out = a_out / std
+        
+        l = [] 
+        phi_in, phi_out = self.phi_correction(a_in, a_out)
+        theta_in, theta_out = self.theta_correction(a_in, a_out)
+        l = l + [phi_in, phi_out, theta_in, theta_out]        
+
+        for hits, ids in [(a_in, self.data.detSeqIn), (a_out, self.data.detSeqOut)]:
+            for id_layer in ids.unique():
+                layer_hits = np.zeros(hits.shape)
+                bool_mask = ids == id_layer
+                layer_hits[bool_mask, :] = hits[bool_mask, :]
+                l.append(layer_hits)
+
+        data = np.array(l) # (channels, batch_size, hit_size)
+        data = data.reshape((len(data), -1, 8, 8))
+        X_hit = np.transpose(data, (1, 2, 3, 0))  
+        
+        X_info = self.get_info_features()
+        y = to_categorical(self.get_labels())
+        return X_hit, X_info, y
+
     def get_labels(self):
         return self.data[target_lab].as_matrix() != 0.0
 
-    def get_data(self, normalize=True):
-        X_hit = self.get_hit_shapes(normalize=normalize)
+    def get_data(self, normalize=True, angular_correction=True, flipped_channels=True):
+        X_hit = self.get_hit_shapes(normalize, angular_correction, flipped_channels)
         X_info = self.get_info_features()
         y = to_categorical(self.get_labels())
         return X_hit, X_info, y
@@ -157,22 +195,30 @@ class Dataset:
 
         n_pos = data_pos.shape[0]
         n_neg = data_neg.shape[0]
+        
         if verbose:
             print("Number of negatives: " + str(n_neg))
             print("Number of positive: " + str(n_pos))
             print("ratio: " + str(n_neg / n_pos))
-        
+
+        if n_pos > n_neg:
+            return self
+
         data_neg = data_neg.sample(n_pos)
         balanced_data = pd.concat([data_neg, data_pos])
-        balanced_data = balanced_data.sample(frac=1)  # Shuffle the dataset again
+        balanced_data = balanced_data.sample(frac=1)  # Shuffle the dataset
         self.data = balanced_data
+        return self  # allow method chaining
 
 
 if __name__ == '__main__':
     d = Dataset('data/debug.npy')
     batch_size = d.data.as_matrix().shape[0]
 
-    x = d.get_data(normalize=False)[0]
+    x = d.get_data()
+    assert x[0].shape == (batch_size, 8, 8, 8)
+
+    x = d.get_data(normalize=False, angular_correction=False, flipped_channels=False)[0]
     assert x.shape == (batch_size, 8, 8, 2)
     np.testing.assert_allclose(x[:, :,:,0], d.data[inhitlabs].as_matrix().reshape((-1, 8, 8)))
 
